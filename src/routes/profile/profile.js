baea0a84eb9d2f44aa10e56ff01919a37435c73a
Anupam Ashish Minz <anupamminz300am@gmail.com>
2021-04-29 01:06:39 +0530

    get username

--- src/routes/profile/profile.js
+++ src/routes/profile/profile.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Profile = () => {
+  const [user, setUser] = useState(null);
+  useEffect(()=>{
+    fetch("/api/loginUser")
+      .then(data => data.json())
+      .then(data => setUser(data));
+  }, [])
+
   return (
     <div>
       profile
+      <div>user {user}</div>
       <form action="/api/logout" method="post">
       <button type="submit">logout</button>
       </form>
